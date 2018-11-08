5e0bc4ef21ad95fe80a0e82d20afa648a0f7e048
Matthew Kelly <mkonabike@gmail.com>
2018-11-08 13:24:52 -0600

    added functions to check if a string is a palindrome, added functions to return a reversed int with math.sign

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ reverseString = (str) => {
 }
 
 let str = 'Matt'
-console.log(reverseString('Matt'))
+// console.log(reverseString('Matt'))
 
 // SOLUTION 2:	USE SPLIT METHOD TO SEPARATE STRING BY CHARACTER
 // AND PUT IN ARRAY, THEN REVERSE THE ARRAY, AND JOIN THE CHARACTERS
@@ -24,8 +24,10 @@ reverseString2 = (str) => {
 
 let jon = 'Jon'
 
-console.log(reverseString2(jon))
+// console.log(reverseString2(jon))
 
+// SOLUTION 3:	USE A FOR LOOP STARTING AT ZERO, BUILD THE EMPTY
+// STRING AND ADD THE NEXT STRING CHARACTER TO THE BEGINNING OF THE STRING
 reverseString3 = (str) => {
 	let newStr = ''
 	for (let i = 0; i < str.length; i++) {
@@ -35,8 +37,9 @@ reverseString3 = (str) => {
 }
 
 let karma = 'Karma'
-console.log(reverseString3(karma))
+// console.log(reverseString3(karma))
 
+// SOLUTION 4: USE A FOR OF LOOP
 reverseString4 = (str) => {
 	let revStr = ''
 	for (let x of str) {
@@ -46,8 +49,9 @@ reverseString4 = (str) => {
 }
 
 let marley = 'Marley'
-console.log(reverseString4(marley))
+// console.log(reverseString4(marley))
 
+// SOLUTION 5: USE A SPLIT METHOD AND A FOR EACH LOOP ON THE STRING ARRAY
 reverseString5 = (str) => {
 	let revStr = ''
 	str.split('').forEach(char => {
@@ -56,4 +60,56 @@ reverseString5 = (str) => {
 	return revStr
 }
 let boomer = 'Boomer'
-console.log(reverseString5(boomer))
+// console.log(reverseString5(boomer))
+
+
+// CHALLENGE 2: VALIDATE A PALINDROME
+// RETURN TRUE IF STRING IS A PALIDNDROME AND FALSE IF NOT
+
+isPalidndrome = (str) => {
+	if (reverseString2(str) === str) {
+		return true
+	}
+	else {
+		return false
+	}
+}
+
+// console.log(isPalidndrome('bob'))
+// console.log(isPalidndrome('bobby'))
+
+isPalidndrome2 = (str) => {
+	let revStr = str.split('').reverse().join('')
+	if (revStr === str) {
+		return true
+	}
+	else {
+		return false
+	}
+}
+
+console.log(isPalidndrome2('mom'))
+console.log(isPalidndrome2('mommy'))
+
+// CHALLENGE 3: REVERSE AND INTERGER
+// RETURN AN INTERGER IN REVERSE AS AN INTEGER
+
+reverseInt = (int) => {
+	let revInt = reverseString2(int.toString())
+	return parseInt(revInt)
+}
+
+// console.log(reverseInt(3672))
+
+reverseInt2 = (int) => {
+	let revInt = int.toString().split('').reverse().join('')
+	return parseInt(revInt)
+}
+// console.log(reverseInt2(3672))
+
+// REVERSE STAYS THE SAME IF INT IS POSITIVE OR NEGATIVE
+reverseInt3 = (int) => {
+	let revInt = int.toString().split('').reverse().join('')
+	return parseInt(revInt) * Math.sign(int)
+}
+console.log(reverseInt3(-37359))

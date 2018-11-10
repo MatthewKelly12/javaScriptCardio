14560a83a371cc11b81d61d5afc383f2e6b406b4
Matthew Kelly <mkonabike@gmail.com>
2018-11-10 14:39:26 -0600

    added functions to check for palindrom and  reverse integer

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ reverseString = (str) => {
 	return newString
 }
 
-let str = 'Matt'
+let matt = 'Matt'
 // console.log(reverseString('Matt'))
 
 // SOLUTION 2:	USE SPLIT METHOD TO SEPARATE STRING BY CHARACTER
@@ -66,6 +66,8 @@ let boomer = 'Boomer'
 // CHALLENGE 2: VALIDATE A PALINDROME
 // RETURN TRUE IF STRING IS A PALIDNDROME AND FALSE IF NOT
 
+
+// SOLUTION 1: USE FUNCTION FOR REVERSE STRING AND COMPARE W/IF STATEMENT
 isPalidndrome = (str) => {
 	if (reverseString2(str) === str) {
 		return true
@@ -78,6 +80,9 @@ isPalidndrome = (str) => {
 // console.log(isPalidndrome('bob'))
 // console.log(isPalidndrome('bobby'))
 
+
+// SOLUTION 2: USE SPLIT TO MAKE AN ARRAY OF STRING,
+//             THEN REVERSE THE ARRAY, AND JOIN AT QUOTES
 isPalidndrome2 = (str) => {
 	let revStr = str.split('').reverse().join('')
 	if (revStr === str) {
@@ -88,12 +93,14 @@ isPalidndrome2 = (str) => {
 	}
 }
 
-console.log(isPalidndrome2('mom'))
-console.log(isPalidndrome2('mommy'))
+// console.log(isPalidndrome2('mom'))
+// console.log(isPalidndrome2('mommy'))
 
 // CHALLENGE 3: REVERSE AND INTERGER
 // RETURN AN INTERGER IN REVERSE AS AN INTEGER
 
+
+// SOLUTION 1: USE FUNCTION FOR REVERSE STRING AND  TO STRING METHOD
 reverseInt = (int) => {
 	let revInt = reverseString2(int.toString())
 	return parseInt(revInt)
@@ -101,15 +108,20 @@ reverseInt = (int) => {
 
 // console.log(reverseInt(3672))
 
+
+// SOLUTION 2: USE METHODS FOR TO STRING, SPLIT, REVERSE, AND JOIN
 reverseInt2 = (int) => {
 	let revInt = int.toString().split('').reverse().join('')
 	return parseInt(revInt)
 }
 // console.log(reverseInt2(3672))
 
+
+// SOLUTION 3: USE MATH.SIGN(INT) TO KEEP + OR -
 // REVERSE STAYS THE SAME IF INT IS POSITIVE OR NEGATIVE
 reverseInt3 = (int) => {
 	let revInt = int.toString().split('').reverse().join('')
 	return parseInt(revInt) * Math.sign(int)
 }
-console.log(reverseInt3(-37359))
+// console.log(reverseInt3(-37359))
+

c07a0138b85cd5d6e11e14bc77d131cd5e3bef6b
Matthew Kelly <mkonabike@gmail.com>
2018-11-11 09:06:09 -0600

    added four methods for capitalizing the first letter of any string

--- capitalizeLetters.js
+++ capitalizeLetters.js
@@ -10,8 +10,8 @@ capitlizeFirstLetter = (str) => {
 	let newSent = str.toLowerCase().split(' ')
 	let capString = ''
 
-	newSent.forEach(char => {
-		capString += char.substring(0,1).toUpperCase() + char.substring(1) + ' '
+	newSent.forEach(word => {
+		capString += word.substring(0,1).toUpperCase() + word.substring(1) + ' '
 	})
 
 	return capString
@@ -19,10 +19,42 @@ capitlizeFirstLetter = (str) => {
 
 const sent = 'HEy whAts up?'
 
-console.log(capitlizeFirstLetter(sent))
+// console.log(capitlizeFirstLetter(sent))
 
 
-// SOLUTION 2: FOR LOOP
+// SOLUTION 2: SPLIT METHOD AND FOR LOOP OVER ARRAY, USE SUBSTRING ON FIRST
+// LETTER OF WORD, CAPITALIZE IT, AND ADD A SUBSTRING OF THE REST OF THE WORD
+// RETURN ARRAY WITH JOIN METHOD AND A SPACE
 capitlizeFirstLetter2 = (str) => {
 
+	const strArray = str.toLowerCase().split(' ')
+	for (let i = 0; i < strArray.length; i++) {
+		strArray[i] = strArray[i].substring(0,1).toUpperCase() + strArray[i].substring(1)
+	}
+	return strArray.join(' ')
 }
+
+// console.log((capitlizeFirstLetter2(sent)))
+
+// SOLUTION 3: LOWERCASE ENTIRE STRING AND SPLIT TO AN ARRAY,
+// USE MAP METHOD OVER ARRAY TO RETURN A CAPITILIZED FIRST LETTTER
+// AND SUBSTRING OF REST OF WORD, USE JOIN METHOD WITH A SPACE
+capitlizeFirstLetter3 = (str) => {
+	return str.toLowerCase().split(' ')
+	.map(word => {
+		return word[0].toUpperCase() + word.substring(1)
+	})
+	.join(' ')
+}
+
+// console.log(capitlizeFirstLetter3(sent))
+
+// SOLUTION 4: REGULAR EXPRESSIONS
+
+capitlizeFirstLetter4 = (str) => {
+	return str.toLowerCase().replace(/\b[a-z]/gi, function(char) {
+		return char.toUpperCase()
+	})
+}
+
+// console.log(capitlizeFirstLetter4(sent))
